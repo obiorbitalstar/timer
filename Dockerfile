65aedb4d096af0026bde0be84cf6754fcf7deb94
obiorbitalstar <obiorbitalstar@gmail.com>
2024-05-22 12:02:15 +0300

    visual update

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,18 @@
-# Stage 1: Build the application
 FROM node:22 AS build
 
-# Create app directory
 WORKDIR /usr/src/app
 
-# Install app dependencies
 COPY package*.json ./
 RUN npm install
 
-# Bundle app source
 COPY . .
 
-# Stage 2: Run the application
 FROM node:22-alpine
 
-# Create app directory
 WORKDIR /usr/src/app
 
-# Copy only the necessary files from the build stage
 COPY --from=build /usr/src/app .
 
-# Expose the port the app runs on
 EXPOSE 3000
 
-# Command to run the app
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     const timestamp = new Date().toISOString();
-    res.send(`Hello World! Current timestamp: ${timestamp}`);
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>DevOps Task</title>
+            <style>
+                body {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100vh;
+                    margin: 0;
+                    background-color: white;
+                }
+                .container {
+                    text-align: center;
+                }
+                .logo {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+                .logo img {
+                    max-width: 100%;
+                    height: auto;
+                }
+                .logo-text {
+                    font-size: 39px;
+                    font-weight: bold;
+                    margin-left: -44px;
+                }
+                .timestamp {
+                    font-weight: bold;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="logo">
+                    <img src="/kabi.png" alt="Kabi Logo">
+                    <div class="logo-text">DevOps Task</div>
+                </div>
+                <div class="timestamp">Current timestamp: ${timestamp}</div>
+            </div>
+        </body>
+        </html>
+    `);
 });
 
 app.listen(port, () => {
